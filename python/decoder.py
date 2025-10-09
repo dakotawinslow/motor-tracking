0917c408b40f35dd12de27c8c374ecd15232b70e
Dakota <dakotawinslow@gmail.com>
2025-10-09 19:21:14 -0400

    Started work on despreading

--- python/decoder.py
+++ python/decoder.py
@@ -1,7 +1,9 @@
 import numpy as np
 from scipy import signal, io, fft
+import json
 import matplotlib.pyplot as plt
 import tone_generator as tg
+
 
 offset = 0  # number of samples off from perfectly in-phase, for simulating issues
 
@@ -39,17 +41,33 @@ def demodulate_FSK(sig: np.ndarray):
             low_peak = int((tg.freq_min / top_freq) * sig_ft.shape[0])
             high_peak = int((tg.freq_max / top_freq) * sig_ft.shape[0])
         if sig_ft[low_peak] > sig_ft[high_peak]:
-            symbols.append(0)
+            symbols.append(False)
         else:
-            symbols.append(1)
+            symbols.append(True)
         print(f"Low peak value ({tg.freq_min}hz): {sig_ft[low_peak]}")
         print(f"High peak value ({tg.freq_max}hz): {sig_ft[high_peak]}")
         # amp_low = np.mean(amp_low)
         # print(amp_low)
+    symbols = np.array(symbols, np.uint8)
     print(symbols)
     return symbols
 
 
+def despread(symbols, goldcode):
+    PRN = np.array(goldcode, np.uint8)
+    # section signal into code-length chunks
+    codelen = PRN.shape[0]
+    numcodes = symbols.shape[0] // codelen
+    for i in range(numcodes):
+        section = symbols[i : i + codelen]
+        print(f"section: {section}")
+        print(f"PRN: {PRN}")
+        print(f"PRN ^ section: {PRN ^ section}")
+
+
 if __name__ == "__main__":
     rate, sig = io.wavfile.read("signal.wav")
+    with open("goldcodes") as f:
+        goldcodes = json.load(f)
     symbols = demodulate_FSK(sig)
+    despread(symbols, goldcodes[0])
